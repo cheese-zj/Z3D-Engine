5c5be957b28ab94f340a54556deed84c21af2c5b
James Zhao <jzha0385@uni.sydney.edu.au>
2024-03-16 16:26:27 +1100

    Getting shader program/usr/local/bin/python3 /Users/jameszhao/3d-engine-project/main.py

--- model.py
+++ model.py
@@ -4,6 +4,8 @@ class Tri:
     def __init__(self, app):
         self.app = app
         self.ctx = app.ctx
+        self.vbo = self.get_vbo()
+        self.shader_program = self.get_shader_program('default')
 
     def get_vertex(self):
         vertex_data = [(-0.6, -0.8, 0.0), (0.6, -0.8, 0.0), (0.0, 0.8, 0.0)]
@@ -15,3 +17,11 @@ class Tri:
         vbo = self.ctx.buffer(vertex_data)
         return vbo
     
+    def get_shader_program(self, shader_name):
+        with open(f'shaders/{shader_name}.vert') as file:
+            vertex_shader = file.read()
+        with open(f'shaders/{shader_name}.frag') as file:
+            fragment_shader = file.read()
+
+        prog = self.ctx.program(vertex_shader = vertex_shader, fragment_shader=fragment_shader)
+        return prog
